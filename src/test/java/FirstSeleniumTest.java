ba3a3f5edab9e1345fdba16d65624bd8701ce3d5
SofieTkachuk <sofie.tkachuk@gmail.com>
2024-03-03 20:25:54 -0500

    test

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstSeleniumTest {
+
+    private static final String HOME_PAGE_URL = "https://www.facebook.com/";
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void classSetup(){
+        driver = SharedDriver.getWebDriver();
+        driver.get(HOME_PAGE_URL);
+    }
+
+    @AfterAll
+    public static void classTearDown(){
+        SharedDriver.closeDriver();
+    }
+
+    @AfterEach
+    public void testTearDown(){
+        driver.get(HOME_PAGE_URL);
+    }
+
+    @Test
+    public void homePageURLTest() {
+
+        String actualURL = driver.getCurrentUrl();
+        assertEquals(HOME_PAGE_URL, actualURL, "URLs do not match");
+    }
+
+    @Test
+    public void findEmailFieldTest(){
+        //WebElement element = driver.findElement(By.id("email"));
+        //WebElement element = driver.findElement(By.name("email"));
+        //WebElement element = driver.findElement(By.linkText("Создать Страницу"));
+        //WebElement element = driver.findElement(By.partialLinkText("Создать"));
+        //WebElement element = driver.findElement(By.cssSelector("#email"));
+        //WebElement element = driver.findElement(By.className("inputtext"));
+        List<WebElement> element = driver.findElements(By.className("inputtext"));
+        System.out.println(element.size());
+        assertNotNull(element);
+    }
+
+    @Test
+    public void findElementsByXpathTest(){
+        WebElement emailElement = driver.findElement(By.xpath("//input[@name='email']"));
+        assertNotNull(emailElement);
+        WebElement passwordElement = driver.findElement(By.xpath("//input[@data-testid='royal_pass']"));
+        assertNotNull(passwordElement);
+        WebElement loginButtonElement = driver.findElement(By.xpath("//button[@type='submit']"));
+        assertNotNull(loginButtonElement);
+        WebElement forgotPassElement = driver.findElement(By.xpath("//a[text()='Забыли пароль?']"));
+        assertNotNull(forgotPassElement);
+        WebElement createNewAccButton = driver.findElement(By.xpath("//*[text()='Создать новый аккаунт']"));
+        assertNotNull(createNewAccButton);
+    }
+
+    @Test
+    public void loginScreenTest(){
+        WebElement emailElement = driver.findElement(By.xpath("//input[@name='email']"));
+        assertNotNull(emailElement);
+        emailElement.sendKeys("jtsonyajt@gmail.com");
+        String emailValue = emailElement.getAttribute("value");
+        assertEquals("jtsonyajt@gmail.com", emailValue);
+
+        WebElement passwordElement = driver.findElement(By.xpath("//input[@data-testid='royal_pass']"));
+        assertNotNull(passwordElement);
+        passwordElement.sendKeys("PleaseBe2023!");
+        String passValue = passwordElement.getAttribute("value");
+        assertEquals("PleaseBe2023!", passValue);
+
+        WebElement loginButtonElement = driver.findElement(By.xpath("//button[@type='submit']"));
+        assertNotNull(loginButtonElement);
+        loginButtonElement.click();
+    }
+}
---
+++ src/test/java/M10.java
@@ -0,0 +1,184 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class M10 {
+    private static final String HOME_PAGE_URL = "https://www.facebook.com/";
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void classSetup(){
+        driver = SharedDriver.getWebDriver();
+        driver.get(HOME_PAGE_URL);
+    }
+
+    @AfterAll
+    public static void classTearDown(){
+        SharedDriver.closeDriver();
+    }
+
+    //On Facebook main screen find XPath and create a method to press the Create New Account button
+    @Test
+    public void createNewAccButtonTest() throws InterruptedException {
+        WebElement newAccButtonElement = driver.findElement(By.xpath("//a[text()='Создать новый аккаунт']"));
+        assertNotNull(newAccButtonElement);
+        newAccButtonElement.click();
+        Thread.sleep(1000);
+    }
+
+    //Find Xpathes for each text box component and Sign Up button
+    @Test
+    public void elementsByXPath(){
+        WebElement firstNameFieldElement = driver.findElement(By.xpath("//input[@name='firstname']"));
+        assertNotNull(firstNameFieldElement);
+
+        WebElement lastNameFieldElement = driver.findElement(By.xpath("//input[@name='lastname']"));
+        assertNotNull(lastNameFieldElement);
+
+        WebElement phoneNumberOrEmailElement = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+        assertNotNull(phoneNumberOrEmailElement);
+
+        WebElement passElement = driver.findElement(By.xpath("//input[@name='reg_passwd__']"));
+        assertNotNull(passElement);
+
+        WebElement signUpButton = driver.findElement(By.xpath("//button[@name='websubmit']"));
+        assertNotNull(signUpButton);
+    }
+
+    //JUnit tests for account creation page
+    @Test
+    public void accCreationPageTest(){
+        WebElement firstNameFieldElement = driver.findElement(By.xpath("//input[@name='firstname']"));
+        assertNotNull(firstNameFieldElement);
+        firstNameFieldElement.sendKeys("Sofie");
+        String firstNameValue = firstNameFieldElement.getAttribute("value");
+        assertEquals("Sofie", firstNameValue);
+
+        WebElement lastNameFieldElement = driver.findElement(By.xpath("//input[@name='lastname']"));
+        assertNotNull(lastNameFieldElement);
+        lastNameFieldElement.sendKeys("Tkachuk");
+        String lastNameValue = lastNameFieldElement.getAttribute("value");
+        assertEquals("Tkachuk", lastNameValue);
+
+        WebElement phoneNumberOrEmailElement = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+        assertNotNull(phoneNumberOrEmailElement);
+        phoneNumberOrEmailElement.sendKeys("1234@gmail.com");
+        String emailValue = phoneNumberOrEmailElement.getAttribute("value");
+        assertEquals("1234@gmail.com", emailValue);
+
+//        WebElement phoneNumberOrEmailElement = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+//        assertNotNull(phoneNumberOrEmailElement);
+//        phoneNumberOrEmailElement.sendKeys("+1647895437");
+//        String phoneValue = phoneNumberOrEmailElement.getAttribute("value");
+//        assertEquals("+1647895437", phoneValue);
+
+        WebElement passElement = driver.findElement(By.xpath("//input[@name='reg_passwd__']"));
+        assertNotNull(passElement);
+        passElement.sendKeys("14568pass");
+        String passValue = passElement.getAttribute("value");
+        assertEquals("14568pass", passValue);
+
+        WebElement signUpButton = driver.findElement(By.xpath("//button[@name='websubmit']"));
+        assertNotNull(signUpButton);
+        signUpButton.click();
+        driver.get(HOME_PAGE_URL);
+    }
+
+    //Think about long text testing, special character testing, invalid inputs (email), etc. For now, don`t test the error messages, just validate that the new screen was not opened.
+    @Test
+    public void longTextTesting() throws InterruptedException {
+        WebElement newAccButtonElement = driver.findElement(By.xpath("//a[text()='Создать новый аккаунт']"));
+        assertNotNull(newAccButtonElement);
+        newAccButtonElement.click();
+        Thread.sleep(1000);
+
+        WebElement firstNameFieldElement = driver.findElement(By.xpath("//input[@name='firstname']"));
+        assertNotNull(firstNameFieldElement);
+        firstNameFieldElement.sendKeys("Sofiesofiesofiesofie");
+        String firstNameValue = firstNameFieldElement.getAttribute("value");
+        assertEquals("Sofiesofiesofiesofie", firstNameValue);
+
+        WebElement lastNameFieldElement = driver.findElement(By.xpath("//input[@name='lastname']"));
+        assertNotNull(lastNameFieldElement);
+        lastNameFieldElement.sendKeys("Tkachuksofiesofiesofie");
+        String lastNameValue = lastNameFieldElement.getAttribute("value");
+        assertEquals("Tkachuksofiesofiesofie", lastNameValue);
+
+        WebElement phoneNumberOrEmailElement = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+        assertNotNull(phoneNumberOrEmailElement);
+        phoneNumberOrEmailElement.sendKeys("1234sofiesofie@gmail.com");
+        String emailValue = phoneNumberOrEmailElement.getAttribute("value");
+        assertEquals("1234sofiesofie@gmail.com", emailValue);
+
+        WebElement passElement = driver.findElement(By.xpath("//input[@name='reg_passwd__']"));
+        assertNotNull(passElement);
+        passElement.sendKeys("14568pass123456passpasspasspass");
+        String passValue = passElement.getAttribute("value");
+        assertEquals("14568pass123456passpasspasspass", passValue);
+
+        assertEquals(HOME_PAGE_URL, driver.getCurrentUrl());
+        driver.get(HOME_PAGE_URL);
+    }
+
+    @Test
+    public void specialCharactersInvalidInputTesting() throws InterruptedException {
+        WebElement newAccButtonElement = driver.findElement(By.xpath("//a[text()='Создать новый аккаунт']"));
+        assertNotNull(newAccButtonElement);
+        newAccButtonElement.click();
+        Thread.sleep(1000);
+
+        WebElement firstNameFieldElement = driver.findElement(By.xpath("//input[@name='firstname']"));
+        assertNotNull(firstNameFieldElement);
+        firstNameFieldElement.sendKeys("Sofie@$");
+        String firstNameValue = firstNameFieldElement.getAttribute("value");
+        assertEquals("Sofie@$", firstNameValue);
+
+        WebElement lastNameFieldElement = driver.findElement(By.xpath("//input[@name='lastname']"));
+        assertNotNull(lastNameFieldElement);
+        lastNameFieldElement.sendKeys("Tkachuk#@");
+        String lastNameValue = lastNameFieldElement.getAttribute("value");
+        assertEquals("Tkachuk#@", lastNameValue);
+
+        WebElement phoneNumberOrEmailElement = driver.findElement(By.xpath("//input[@name='reg_email__']"));
+        assertNotNull(phoneNumberOrEmailElement);
+        phoneNumberOrEmailElement.sendKeys("**&sofiesofie@gmail.com");
+        String emailValue = phoneNumberOrEmailElement.getAttribute("value");
+        assertEquals("**&sofiesofie@gmail.com", emailValue);
+
+        WebElement passElement = driver.findElement(By.xpath("//input[@name='reg_passwd__']"));
+        assertNotNull(passElement);
+        passElement.sendKeys("1234!#&");
+        String passValue = passElement.getAttribute("value");
+        assertEquals("1234!#&", passValue);
+
+        assertEquals(HOME_PAGE_URL, driver.getCurrentUrl());
+        driver.get(HOME_PAGE_URL);
+    }
+
+    //Don't forget to choose the Custom gender field to verify new  text boxes are displayed and test these text boxes as well
+    @Test
+    public void customGenderField() throws InterruptedException {
+        WebElement newAccButtonElement = driver.findElement(By.xpath("//a[text()='Создать новый аккаунт']"));
+        assertNotNull(newAccButtonElement);
+        newAccButtonElement.click();
+        Thread.sleep(1000);
+
+        WebElement customGender = driver.findElement(By.xpath("//input[@id='u_1d_6_Ju]"));
+        assertNotNull(customGender);
+        customGender.click();
+    }
+
+    @Test
+    public void newTextBoxesTesting() {
+        WebElement sexField = driver.findElement(By.xpath("//input[@name='custom_gender']"));
+        assertNotNull(sexField);
+        sexField.sendKeys("They/Them");
+        String sexValue = sexField.getAttribute("value");
+        assertEquals("They/Them", sexValue);
+    }
+}
