4990f22e9fcdaf7aec77d9d520977b400ca7133d
SofieTkachuk <sofie.tkachuk@gmail.com>
2024-03-11 21:00:13 -0400

    test

--- src/main/java/SharedDriver.java
+++ src/main/java/SharedDriver.java
@@ -1,7 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.concurrent.TimeUnit;
 
 public class SharedDriver {
 
